8a6bd1b4ee957241e4c131dae50c93d435edd94d
Geert van der Cruijsen <geert@vdcruijsen.net>
2025-05-14 20:18:18 +0200

    multi agent

---
+++ multi-agent/aca-demo-app.AppHost/Program.cs
@@ -0,0 +1,18 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.aca_demo_app_ApiService>("apiservice")
+    .WithHttpsHealthCheck("/health");
+
+// Stateful-less chat UI
+var chatUi = builder.AddContainer("chatui", "yidadaa/chatgpt-next-web:latest")
+       .WithEnvironment("OPENAI_API_KEY", "demo-key")
+       .WithEnvironment("BASE_URL", "http://host.docker.internal:5597")                
+       .WithHttpEndpoint(3000,3000);
+
+// var chatbotUi = builder.AddContainer("chatbot-ui2", "ghcr.io/mckaywrigley/chatbot-ui:main")
+//     .WithEnvironment("NEXT_PUBLIC_OLLAMA_URL", apiService.GetEndpoint("https") + "/v1")
+//     // .WithEnvironment("NEXT_PUBLIC_OPENAI_API_TYPE", "openai")
+//     // .WithEnvironment("NEXT_PUBLIC_DEFAULT_MODEL", "phi3")
+//     .WithHttpEndpoint(3000, 3000);
+
+builder.Build().Run();
